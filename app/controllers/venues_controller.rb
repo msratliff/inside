6914d76c757e3be34b1f874da8eafebf59cd2796
Mark Ratliff <mark.s.ratliff@gmail.com>
2017-08-24 11:05:55 -0400

    Added methods to venue controller, added geocoding to venue model, added relationship to venue model

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,35 @@
 class VenuesController < ApplicationController
+
+	before_action :set_venue, only: [:show, :edit, :update, :destroy]
+
+	def index
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def set_venue
+	end
+
+	def venue_params
+	end
+
+
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,12 @@
 class Venue < ApplicationRecord
+
+	has_many :promotions
+	
+	geocoded_by :address
+  after_validation :geocode, if: ->(obj){ obj.city.present? and obj.city_changed? }
+
+  def address
+  	[street, city, state, zipcode, 'USA'].compact.join(', ')
+  end
+  
 end
