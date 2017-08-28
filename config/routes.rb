b19e79fbd6d92fd4325e033a7197c53c1856bbda
Matthew Lombana <mplombana@gmail.com>
2017-08-28 10:12:47 -0400

    changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   root to: "venues#index"
 
   namespace :users do
@@ -18,7 +18,8 @@ Rails.application.routes.draw do
     resources :promotions
   end
 
+  get 'signup' => 'venues#new'
   resources :users
-  
+
 
 end
