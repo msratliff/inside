f231b6fb7084f30de9d122ea7711298915b01d55
Mark Ratliff <mark.s.ratliff@gmail.com>
2017-08-31 09:42:06 -0400

    turned venue logic back on in nav bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authorize, only: [:show, :edit, :update, :destroy]
 
   def show
   end
