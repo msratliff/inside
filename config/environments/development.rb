65faeca392317ee8ea3d6de02c163f6c513d6df3
Matthew Lombana <mplombana@gmail.com>
2017-09-02 16:35:52 -0400

    added mobile styling to logins/forms, desktop mediaqueries still need adjustment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  # Suppress logger output for asset requests.
+  # Suppress logger output for asset requests.x
   config.assets.quiet = true
 
   # Raises error for missing translations
