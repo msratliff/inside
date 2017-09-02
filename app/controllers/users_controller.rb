68c1ff3430b8b73673282b36519c15da0b15a681
Einar Montalvo <einar.mn2407@gmail.com>
2017-09-02 07:56:39 -0400

    Original configuration for Stripe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,6 +62,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :stripeToken)
     end
   end
