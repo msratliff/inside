215ed67c184f18818d421cbaa623bfdac5d0e520
Mark Ratliff <mark.s.ratliff@gmail.com>
2017-09-03 13:16:44 -0400

    updated seed file for new fields

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@
 
 
 User.destroy_all
-Venue.destroy_all
 Promotion.destroy_all
+Venue.destroy_all
 
 
 3.times do |user|
@@ -19,8 +19,8 @@ end
 weekdays = ["Thursday", "Friday", "Saturday"]
 
 3.times do |venue|
-	venue = Venue.create(name: Faker::Ancient.hero, email: Faker::Internet.free_email, password_digest: Faker::Internet.password(5, 6), street: "78 NW 25th St", city: "Miami", state: "FL", zipcode: 33127)
+	venue = Venue.create(name: Faker::Ancient.hero, email: Faker::Internet.free_email, password_digest: Faker::Internet.password(5, 6), street: "78 NW 25th St", city: "Miami", state: "FL", zipcode: 33127, description: Faker::MostInterestingManInTheWorld.quote)
 	weekdays.each do |weekday|
-		promotion = Promotion.create(day_of_week: weekday, time: Faker::Time.between(2.days.ago, Date.today, :afternoon), price: Faker::Commerce.price, venue_id: venue.id)
+		promotion = Promotion.create(name: Faker::GameOfThrones.character, day_of_week: weekday, time: Faker::Time.between(2.days.ago, Date.today, :afternoon), price: Faker::Commerce.price, venue_id: venue.id, description: Faker::ChuckNorris.fact)
 	end
 end
