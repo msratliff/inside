b1cc540557d5ee555dd594e81747ad73d26abe47
Mark Ratliff <mark.s.ratliff@gmail.com>
2017-09-04 23:22:23 -0400

    Adjusted for postgresql db

--- db/migrate/20170824154755_change_lat_longto_float.rb
+++ db/migrate/20170824154755_change_lat_longto_float.rb
@@ -1,6 +1,9 @@
 class ChangeLatLongtoFloat < ActiveRecord::Migration[5.1]
   def change
-  	change_column :venues, :latitude, :float
-  	change_column :venues, :longitude, :float
+  	# change_column :venues, :latitude, :float
+  	execute 'ALTER TABLE "venues" ALTER COLUMN "latitude" TYPE decimal USING latitude::numeric'
+  	# change_column :venues, :longitude, :float
+  	execute 'ALTER TABLE "venues" ALTER COLUMN "longitude" TYPE decimal USING longitude::numeric'
+
   end
 end
